519cc44291bab46eac6ac9c75997087c7d5c9ff9
Mahdi Yazdani <mahdiyazdani@msn.com>
2022-09-11 01:02:29 +0300

    Removed unused rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,9 +2,7 @@ module.exports = {
 	root: true,
 	extends: [ 'plugin:@wordpress/eslint-plugin/recommended' ],
 	rules: {
-		'@wordpress/no-unsafe-wp-apis': 'off',
 		'import/no-unresolved': 'off',
 		'jsdoc/check-line-alignment': 'off',
-		'no-nested-ternary': 'off',
 	},
 };
